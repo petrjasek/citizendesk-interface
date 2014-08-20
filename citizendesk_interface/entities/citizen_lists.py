e022cb91f41b18cdda0c41cade2c7f1a7f50a6b9
danse <f.occhipinti@gmail.com>
2014-08-20 11:07:04 +0200

    this should work

--- citizendesk_interface/entities/citizen_lists.py
+++ citizendesk_interface/entities/citizen_lists.py
@@ -3,21 +3,9 @@ entity = {
         'name': {
             'type': 'string',
         },
-        'type': {
+        'description': {
             'type': 'string',
-            'allowed': ['mobile', 'twitter']
         },
-        'citizens': {
-            'type': 'list',
-            'schema': {
-                'type': 'objectid',
-                'data_relation': {
-                    'resource': 'citizen_aliases',
-                    'field': '_id',
-                    'embeddable': True
-                }
-            }
-        }
     },
     'resource_methods': ['GET', 'POST'],
     'item_methods': ['GET', 'PUT', 'DELETE', 'PATCH'],
