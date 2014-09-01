a9d59539b45df44888574507de2c77a51f5b13a3
danse <f.occhipinti@gmail.com>
2014-09-01 17:28:20 +0200

    add the `direct` status property

--- initialise/report_statuses.py
+++ initialise/report_statuses.py
@@ -2,18 +2,23 @@ from main import Init, init_collection
 
 init_report_statuses = Init('report_statuses', [{
     'key': 'new',
+    'direct': False,
     'description': 'This report is not verified yet'
 }, {
     'key': 'assigned',
+    'direct': False,
     'description': 'This report has been assigned for verification'
 }, {
     'key': 'dismissed',
-    'description': ''
+    'direct': True,
+    'description': 'This report will not undergo verification'
 }, {
     'key': 'false',
+    'direct': True,
     'description': 'This report has been proven to be false!'
 }, {
     'key': 'verified',
+    'direct': True,
     'description': 'This report has been verified'
 }])
 
